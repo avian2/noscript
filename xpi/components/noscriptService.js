630017a528c81ae15a525f41eddff04e10709c02
Tomaz Solc <tomaz.solc@tablix.org>
2011-11-10 12:40:05 +0100

    NoScript addons.mozilla.org release 2.1.9
    v 2.1.9
    ==========================================================================
    x [Surrogate] fixed breakage caused by "1.8.1" JavaScript version spec
      used instead of "1.8"

--- xpi/components/noscriptService.js
+++ xpi/components/noscriptService.js
@@ -5,7 +5,7 @@ const Cc = Components.classes;
 const Cu = Components.utils;
 const Cr = Components.results;
 
-const VERSION = "2.1.9rc4";
+const VERSION = "2.1.9";
 const SERVICE_CTRID = "@maone.net/noscript-service;1";
 const SERVICE_ID = "{31aec909-8e86-4397-9380-63a59e0c5ff5}";
 const EXTENSION_ID = "{73a6fe31-595d-460b-a920-fcc0f8843232}";
