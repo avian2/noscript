160c8f08d49a040cebde0ef701e8074b555cd8a1
Tomaz Solc <tomaz.solc@tablix.org>
2014-01-15 05:15:09 +0100

    NoScript addons.mozilla.org release 2.6.8.12rc4
    v 2.6.8.12rc4
    =========================================================================
    x Improved work-around for
      https://bugzilla.mozilla.org/show_bug.cgi?id=958962
    + [Surrogate] Prevent blank ModPagespeed-patched pages when meta refresh
      inside NOSCRIPT elements is blocked (thanks  thunderscript and barbaz)

--- xpi/chrome/content/noscript/WinScript.js
+++ xpi/chrome/content/noscript/WinScript.js
@@ -6,32 +6,33 @@ var WinScript = ("blockScriptForGlobal" in Cu)
     Cu.blockScriptForGlobal(window);
     window._blockScriptForGlobal = true;
     // work-around for https://bugzilla.mozilla.org/show_bug.cgi?id=958962
-    window.addEventListener("DOMContentLoaded", this._onContentLoaded, false);
+    this._patchStyle(window.document);
   },
   unblock: function(window) {
     if (!window._blockScriptForGlobal) return;
     Cu.unblockScriptForGlobal(window);
     window._blockScriptForGlobal = false;
   },
-  _onContentLoaded: function(ev) {
-    let win = ev.currentTarget;
-    let doc = win.document;
-    let nodes = doc.getElementsByTagName("noscript");
-    if (win.getComputedStyle(nodes[0], '').display !== "none") return;
-    if (!this._sheetAdded) {
-      ns.updateStyleSheet('noscript[data-noscript-visible] { display: inline !important}', true);
-      this._sheetAdded = true;
+  _domUtils: Cc["@mozilla.org/inspector/dom-utils;1"].getService(Ci.inIDOMUtils),
+  _patchStyle: function(doc) {
+    let ss = this._domUtils.getAllStyleSheets(doc);
+    // reverse loop because the preference stylesheet is almost always the last one
+    for (let j = ss.length; j-- > 0;) { 
+      let s = ss[j];
+      if(s.href === "about:PreferenceStyleSheet") {
+          let rules = s.cssRules;
+          // skip 1st & 2nd, as they are HTML & SVG namespaces
+          for (let j = 2, len = rules.length; j < len; j++) {
+              let r = rules[j];
+              if (r.cssText === "noscript { display: none ! important; }") {
+                  s.deleteRule(j);
+                  break;
+              }
+          }
+          break;
+      }
     }
-    let range = doc.createRange();
-    for (let j = 0; j < nodes.length; j++) {
-      let n = nodes[j];
-      range.selectNode(n.parentNode);
-      let f = range.createContextualFragment(n.innerHTML)
-      n.innerHTML = "";
-      n.appendChild(f);
-      n.setAttribute("data-noscript-visible", "true");
-    }
-  }
+ }
 }
 :
 {
