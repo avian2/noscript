105ea6e480164dd80843b2c71881b75c8e8a3b7f
Tomaz Solc <tomaz.solc@tablix.org>
2015-04-20 21:13:05 +0200

    NoScript addons.mozilla.org release 2.6.9.22
    v 2.6.9.22
    =============================================================
    + [Surrogate] Generalized OWASP antiClickjacking replacement
      (thanks barbaz for RFE)
    + [Surrogate] Wordpress scriptless site auto-show replacement
    + bootstrapcdn.com in default whitelist

--- xpi/components/noscriptService.js
+++ xpi/components/noscriptService.js
@@ -2119,7 +2119,7 @@ var ns = {
       "2.6.9.19rc1": {
         "youtube.com": "googlevideo.com" // Youtube's HTML5 video player now requires this
       },
-      "@VERSION@": {
+      "@VERSION@rc1": {
         "prototypejs.org": "bootstrapcdn.com" // Used by many sites, mostly for styles and fonts
       }
     };
